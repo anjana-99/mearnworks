b48af0623d73df8f6d305925956232fb30687b17
Anjana <anjanamanakkal@gmail.com>
2021-09-13 11:09:12 +0530

    modified

---
+++ JAVASCRIPT/operators.js
@@ -0,0 +1,25 @@
+// console.log(50>10);
+
+// console.log(60>100>110);
+
+
+// var num=9;
+
+// if(num<0)
+// console.log("neagative");
+
+// if(num>0)
+// console.log("positive");
+
+
+var num1=10;
+var num2=10;
+
+if(num1>num2)
+console.log(num1);
+
+else if(num2>num1)
+console.log(num2);
+
+else
+console.log("equal");
--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -1,2 +1,13 @@
-console.log("hello world");  //outputing a message.
-console.log(hello world);
+// console.log("hello world");  //outputing a message.
+// var pname="anjana";
+// var age=22;
+
+// console.log(pname);
+// console.log(age);
+
+// var comp_n="luminar";
+// var loc_n="kakkanad";
+
+// console.log(`our company ${comp_n} located in ${loc_n}`);
+
+console.log(50<20<10);
